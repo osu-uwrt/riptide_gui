d025eb2ed86caae7c4093fac1b35b7198ec5e2ab
hunter <hunterseachrist@gmail.com>
2023-02-10 19:52:32 -0500

    Add recipe_parser_test

---
+++ riptide_rviz/riptide_rviz/tests/recipe_parser_tests.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "bringup_recipe.hpp"
+
+int main() {
+    std::cout << "--------------------\n";
+    std::cout << "RECIPE PARSER TESTER\n";
+    std::cout << "--------------------\n\n";
+}
