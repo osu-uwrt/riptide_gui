0be5a4fb1fcdd529feb1337ecbe30bca9735af6f
Chris Barlas <barlas.3@osu.edu>
2023-02-23 22:04:01 -0500

    Start and stop button more fully functioning

--- riptide_rviz/riptide_rviz/include/riptide_rviz/BringupClient.hpp
+++ riptide_rviz/riptide_rviz/include/riptide_rviz/BringupClient.hpp
@@ -18,6 +18,8 @@ namespace riptide_rviz
 {
     using BringupStart = launch_msgs::action::BringupStart;
     using GHBringupStart = rclcpp_action::ClientGoalHandle<BringupStart>;
+    using BringupEnd = launch_msgs::action::BringupEnd;
+    using GHBringupEnd = rclcpp_action::ClientGoalHandle<BringupEnd>;
 
     class BringupClient : public QWidget
     {
@@ -30,11 +32,16 @@ namespace riptide_rviz
             Ui_BringupListElement *listElement;
             rclcpp::Node::SharedPtr clientNode;
             rclcpp_action::Client<BringupStart>::SharedPtr bringupStart;
+            rclcpp_action::Client<BringupEnd>::SharedPtr bringupEnd;
             std::shared_ptr<riptide_rviz::RecipeLaunch> recipeLaunchData;
+            int pid;
             void BU_start_goal_response_cb(const GHBringupStart::SharedPtr & goal_handle);
             void BU_start_feedback_cb(GHBringupStart::SharedPtr, const std::shared_ptr<const BringupStart::Feedback> feedback);
             void BU_start_result_cb(const GHBringupStart::WrappedResult & result);
             void startButtonCallback();
-
+            void stopButtonCallback();
+            void BU_end_goal_response_cb(const GHBringupEnd::SharedPtr & goal_handle);
+            void BU_end_feedback_cb(GHBringupEnd::SharedPtr, const std::shared_ptr<const BringupEnd::Feedback> feedback);
+            void BU_end_result_cb(const GHBringupEnd::WrappedResult & result);
     };
 }
--- riptide_rviz/riptide_rviz/src/BringupClient.cpp
+++ riptide_rviz/riptide_rviz/src/BringupClient.cpp
@@ -13,12 +13,64 @@ namespace riptide_rviz
 
         clientNode = parentNode;
         recipeLaunchData = recipeLaunch;
+        
+        //Remove .launch.py to save space
+        std::string nameCopy = recipeLaunch->name;
+        size_t indexOfExt = nameCopy.find(".launch.py");
+        if(indexOfExt != std::string::npos)
+        {
+            nameCopy.erase(indexOfExt, 10);
+        }
+        QString launchFileName = QString::fromStdString(nameCopy);
+        listElement->launchFileLabel->setText(launchFileName);
+
+        //Change format of progress bar
+        auto topicCount = recipeLaunch->topicList.size();
+        int topicCountInt = static_cast<int>(topicCount);
+        listElement->progressBar->setMaximum(topicCountInt);
+        listElement->progressBar->setFormat("%v/%m");
 
         //Create two action clients one for bringup start, one for bringup stop
-        RVIZ_COMMON_LOG_DEBUG("BringupClient: Hostname is:" + hostName + "/bringup_start");
+        RVIZ_COMMON_LOG_INFO("BringupClient: Hostname is:" + hostName + "/bringup_start");
         bringupStart = rclcpp_action::create_client<BringupStart>(clientNode, hostName + "/bringup_start");
+        bringupEnd = rclcpp_action::create_client<BringupEnd>(clientNode, hostName + "/bringup_end");
         connect(listElement->startButton, &QToolButton::clicked, [this](void)
             {startButtonCallback(); });
+        connect(listElement->stopButton, &QToolButton::clicked, [this](void)
+            {stopButtonCallback(); });
+    }
+
+    void BringupClient::stopButtonCallback()
+    {
+        listElement->stopButton->setDisabled(true);
+        listElement->progressBar->setValue(0);
+        auto goal_msg = BringupEnd::Goal();
+        goal_msg.pid = pid;
+        auto send_goal_options = rclcpp_action::Client<BringupEnd>::SendGoalOptions();
+        send_goal_options.goal_response_callback = std::bind(&BringupClient::BU_end_goal_response_cb, this, _1);
+        send_goal_options.feedback_callback = std::bind(&BringupClient::BU_end_feedback_cb, this, _1, _2);
+        send_goal_options.result_callback = std::bind(&BringupClient::BU_end_result_cb, this, _1);
+        bringupEnd->async_send_goal(goal_msg, send_goal_options);
+    }
+
+    void BringupClient::BU_end_goal_response_cb(const GHBringupEnd::SharedPtr & goal_handle)
+    {
+        if (!goal_handle) {
+            RVIZ_COMMON_LOG_ERROR("BU_end_goal_response_cb: Server has rejected the goal.");
+        }
+    }
+
+    void BringupClient::BU_end_feedback_cb(GHBringupEnd::SharedPtr, const std::shared_ptr<const BringupEnd::Feedback> feedback)
+    {
+        int killedNodeCount = static_cast<int>(feedback->killed_nodes);
+        RVIZ_COMMON_LOG_INFO("BU_end_goal_response: Killed complete count is " + std::to_string(killedNodeCount));
+        listElement->progressBar->setValue(killedNodeCount);
+    }
+
+    void BringupClient::BU_end_result_cb(const GHBringupEnd::WrappedResult & result)
+    {
+        listElement->progressBar->setValue(listElement->progressBar->maximum());
+        listElement->startButton->setEnabled(true);
     }
 
     //Callback functions for BringupStart Client
@@ -32,12 +84,16 @@ namespace riptide_rviz
 
     void BringupClient::BU_start_feedback_cb(GHBringupStart::SharedPtr, const std::shared_ptr<const BringupStart::Feedback> feedback)
     {
-
+        int completedCount = static_cast<int>(feedback->completed_topics);
+        listElement->progressBar->setValue(completedCount);
     }
 
     void BringupClient::BU_start_result_cb(const GHBringupStart::WrappedResult & result)
     {
-        auto pid = result.result->pid;
+        pid = result.result->pid;
+        RVIZ_COMMON_LOG_INFO("BU_start_result_cb: pid to look for is " + pid);
+        listElement->stopButton->setEnabled(true);
+        listElement->startButton->setDisabled(true);
     }
 
     //Create two callbacks for start and stop buttons
@@ -45,6 +101,8 @@ namespace riptide_rviz
     void BringupClient::startButtonCallback()
     {
         RVIZ_COMMON_LOG_DEBUG("startButtonCallback: Launch file start button pressed");
+        listElement->progressBar->setValue(0);
+
         auto goal_msg = BringupStart::Goal();
         goal_msg.launch_file = recipeLaunchData->name;
         goal_msg.launch_package = recipeLaunchData->package;
@@ -70,6 +128,9 @@ namespace riptide_rviz
         send_goal_options.feedback_callback = std::bind(&BringupClient::BU_start_feedback_cb, this, _1, _2);
         send_goal_options.result_callback = std::bind(&BringupClient::BU_start_result_cb, this, _1);
         bringupStart->async_send_goal(goal_msg, send_goal_options);
+
+        listElement->startButton->setDisabled(true);
+        listElement->stopButton->setDisabled(true);
     }
 
     BringupClient::~BringupClient()
