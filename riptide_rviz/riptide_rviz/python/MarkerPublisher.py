89663dda05779ef1ce7144978d2b2b2a9063d6f3
Brach Knutson <brach2003@gmail.com>
2023-07-19 23:09:22 -0400

    added 2023 markers

--- riptide_rviz/riptide_rviz/python/MarkerPublisher.py
+++ riptide_rviz/riptide_rviz/python/MarkerPublisher.py
@@ -14,10 +14,11 @@ from std_msgs.msg import ColorRGBA
 from transforms3d.euler import euler2quat
 from visualization_msgs.msg import Marker, MarkerArray
 
-UPDATE_PERIOD = 0.25
+
 CONFIG_FILE = os.path.join(get_package_share_directory("riptide_rviz"), "config", "markers.yaml")
 LINEAR_CMD_TOPIC = "controller/linear"
 ANGULAR_CMD_TOPIC = "controller/angular"
+ODOMETRY_TOPIC = "odometry/filtered"
 
 
 def toPoint(v: Vector3) -> Point:
@@ -46,24 +47,26 @@ def poseFromArr(d) -> Pose:
     )
     
     return p
+
+
+class MarkerInfo:
+    def __init__(self, mesh: str, frame: str, scale: float, pose: 'list[float]'):
+        self.mesh   = mesh
+        self.frame  = frame
+        self.scale  = scale
+        self.pose   = pose
     
 
 class MarkerPublisher(Node):
     def __init__(self):
-        super().__init__("MarkerPublisher")
-        self.get_logger().info("Marker Publisher node started.")
-        self.timer = self.create_timer(UPDATE_PERIOD, self.timerCB)
+        super().__init__("marker_publisher")
+        self.readParameters()
+        if self.updatePeriod == 0:
+            self.get_logger().error(f"Failed to read parameters or update rate is 0.")
+            exit(1)
         
-        #configure yaml vars
-        stream = open(CONFIG_FILE, 'r')
-        docRoot = yaml.safe_load(stream)["marker_publisher"]
-        self.meshPkg = docRoot["mesh_pkg"]
-        self.meshDir = docRoot["mesh_directory"]
-        self.markerTopic = docRoot["marker_topic"]
-        self.robot = docRoot["robot"]
-        self.markers = docRoot["markers"]
-        self.markerNames = list(self.markers.keys()) #so swe can iterate through the list with numerical ids
-        
+        self.timer = self.create_timer(self.updatePeriod, self.timerCB)
+
         #configure ghost tempest vars
         self.latestLinearCmd = ControllerCommand()
         self.latestAngularCmd = ControllerCommand()
@@ -73,8 +76,55 @@ class MarkerPublisher(Node):
         self.markerPub = self.create_publisher(MarkerArray, self.markerTopic, 10)
         self.linearSub = self.create_subscription(ControllerCommand, LINEAR_CMD_TOPIC, self.linearCB, 10)
         self.angularSub = self.create_subscription(ControllerCommand, ANGULAR_CMD_TOPIC, self.angularCB, 10)
-        self.odomSub = self.create_subscription(Odometry, "odometry/filtered", self.odomCB, 10)
+        self.odomSub = self.create_subscription(Odometry, ODOMETRY_TOPIC, self.odomCB, 10)
+    
+        self.get_logger().info("Marker Publisher node started.")
         
+    
+    def declareAndReceiveParam(self, name: str, default):
+        self.declare_parameter(name, default)
+        return self.get_parameter(name).value
+    
+    
+    def hasParametersForMarker(self, idx):
+        return \
+            self.markers[idx].mesh != "" and \
+            self.markers[idx].frame != "" and \
+            self.markers[idx].scale > 0
+    
+    
+    def readParameters(self):
+        #declare parameters
+        self.updatePeriod   = self.declareAndReceiveParam("update_period", 0.0)
+        self.meshPkg        = self.declareAndReceiveParam("mesh_pkg", "")
+        self.meshDir        = self.declareAndReceiveParam("mesh_directory", "")
+        self.markerTopic    = self.declareAndReceiveParam("marker_topic", "")
+        self.robot          = self.declareAndReceiveParam("robot", "")
+        
+        markerIdx = 0
+        self.markers: 'list[MarkerInfo]' = [
+            MarkerInfo(
+                self.declareAndReceiveParam("markers.marker0.mesh", ""),
+                self.declareAndReceiveParam("markers.marker0.frame", ""),
+                self.declareAndReceiveParam("markers.marker0.scale", 0.0),
+                self.declareAndReceiveParam("markers.marker0.pose", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+            )
+        ]
+        
+        while(self.hasParametersForMarker(markerIdx)):
+            markerIdx += 1
+            self.markers.append(
+                MarkerInfo(
+                    self.declareAndReceiveParam(f"markers.marker{markerIdx}.mesh", ""),
+                    self.declareAndReceiveParam(f"markers.marker{markerIdx}.frame", ""),
+                    self.declareAndReceiveParam(f"markers.marker{markerIdx}.scale", 0.0),
+                    self.declareAndReceiveParam(f"markers.marker{markerIdx}.pose", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+                )
+            )
+
+        # because of how the while executes, the last marker in self.markers will be bad. delete it
+        self.markers.remove(self.markers[-1])
+    
         
     def linearCB(self, msg):
         self.latestLinearCmd = msg
@@ -90,25 +140,20 @@ class MarkerPublisher(Node):
         
     def timerCB(self):
         array = MarkerArray()
-        for i in range(0, len(self.markerNames)):
-            propName = self.markerNames[i]
-            frame = self.markers[propName]["frame"]
-            mesh = self.markers[propName]["mesh"]
-            pose = self.markers[propName]["pose"]
-            scale = float(self.markers[propName]["scale"])
-            
+        for i in range(0, len(self.markers)):
+            markerInfo = self.markers[i]
             marker = Marker()
-            marker.header.frame_id = frame
+            marker.header.frame_id = markerInfo.frame
             marker.header.stamp = self.get_clock().now().to_msg()
             marker.ns = "mapping_markers"
             marker.id = i
             marker.type = Marker.MESH_RESOURCE
             marker.action = Marker.MODIFY
-            marker.pose = poseFromArr(pose)
-            marker.scale = Vector3(x=scale, y=scale, z=scale)
+            marker.pose = poseFromArr(markerInfo.pose)
+            marker.scale = Vector3(x=markerInfo.scale, y=markerInfo.scale, z=markerInfo.scale)
             marker.lifetime = Duration().to_msg() #forever
             marker.frame_locked = True
-            marker.mesh_resource = "file://" + os.path.join(get_package_share_directory(self.meshPkg), self.meshDir, mesh, "model.dae")
+            marker.mesh_resource = "file://" + os.path.join(get_package_share_directory(self.meshPkg), self.meshDir, markerInfo.mesh, "model.dae")
             marker.mesh_use_embedded_materials = True
             
             array.markers.append(marker)
