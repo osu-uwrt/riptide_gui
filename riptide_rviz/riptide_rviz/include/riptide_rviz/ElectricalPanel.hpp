98e8dfefd05c03fd6d688093f0f227953ed1e3a0
coalman321 <cjtucker321@gmail.com>
2023-07-29 02:36:25 -0400

    add IMU calibration to the electrical panel

--- riptide_rviz/riptide_rviz/include/riptide_rviz/ElectricalPanel.hpp
+++ riptide_rviz/riptide_rviz/include/riptide_rviz/ElectricalPanel.hpp
@@ -1,13 +1,25 @@
 #pragma once
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+
 #include <rviz_common/panel.hpp>
-#include "riptide_msgs2/msg/electrical_command.hpp"
+
+#include <riptide_msgs2/msg/electrical_command.hpp>
+#include <vectornav_msgs/action/mag_cal.hpp>
+
 #include "ui_ElectricalPanel.h"
 
 namespace riptide_rviz
 {
+    const static std::string CALIB_ACTION_NAME = "vectornav/mag_cal";
+
     class ElectricalPanel : public rviz_common::Panel
     {
+        using MagCal = vectornav_msgs::action::MagCal;
+        using MagSendGoalOptions = rclcpp_action::Client<MagCal>::SendGoalOptions;
+        using MagGoalHandle = rclcpp_action::Client<MagCal>::GoalHandle;
+        
+
         Q_OBJECT
         public:
         ElectricalPanel(QWidget *parent = 0);
@@ -19,11 +31,25 @@ namespace riptide_rviz
 
         private Q_SLOTS:
         void sendCommand();
+        void sendMagCal();
 
         private:
+        void goalResponseCb(const MagGoalHandle::SharedPtr & goal_handle);
+        void feedbackCb(
+            MagGoalHandle::SharedPtr,
+            const std::shared_ptr<const MagCal::Feedback> feedback);
+        void resultCb(const MagGoalHandle::WrappedResult & result);
+
+        // electrical command vars
+        bool loaded = false;
         Ui_ElectricalPanel *ui;
         QString robotNs;
+
+        // mag cal vars
+        bool calInProgress = false;
+        double maxVar = 0.0;
+
         rclcpp::Publisher<riptide_msgs2::msg::ElectricalCommand>::SharedPtr pub;
-        bool loaded = false;
+        rclcpp_action::Client<vectornav_msgs::action::MagCal>::SharedPtr imuCalClient;
     };
 }
