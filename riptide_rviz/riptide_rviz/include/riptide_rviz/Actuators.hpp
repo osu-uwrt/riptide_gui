10c1ef3eb9d0640cb094b1de95c4c070971ac445
Brach Knutson <brach2003@gmail.com>
2024-02-14 16:02:40 -0500

    working actuator panel!

--- riptide_rviz/riptide_rviz/include/riptide_rviz/Actuators.hpp
+++ riptide_rviz/riptide_rviz/include/riptide_rviz/Actuators.hpp
@@ -3,10 +3,20 @@
 #include <rviz_common/panel.hpp>
 #include <rviz_common/config.hpp>
 #include <QTimer>
+#include <QMessageBox>
+#include <std_srvs/srv/set_bool.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <riptide_msgs2/msg/actuator_status.hpp>
 #include "ui_Actuators.h"
+
+using namespace std::chrono_literals;
 
 namespace riptide_rviz
 {
+    using Trigger = std_srvs::srv::Trigger;
+    using SetBool = std_srvs::srv::SetBool;
+    using ActuatorStatus = riptide_msgs2::msg::ActuatorStatus;
+
     class Actuators : public rviz_common::Panel
     {
         Q_OBJECT public : Actuators(QWidget *parent = 0);
@@ -24,17 +34,116 @@ namespace riptide_rviz
         void handleCloseClaw();
         void handleFireTorpedo();
         void handleDropMarker();
+        void handleReload();
         void handleClawGoHome();
         void handleClawSetHome();
         void handleTorpGoHome();
         void handleTorpSetHome();
 
     private:
+        void statusCallback(const riptide_msgs2::msg::ActuatorStatus::SharedPtr msg);
+        void updateStatus(const std::string& status);
+        void callTriggerService(rclcpp::Client<Trigger>::SharedPtr client);
+        void callSetBoolService(rclcpp::Client<SetBool>::SharedPtr client, bool value);
+
+        template<typename SrvType>
+        void callService(typename rclcpp::Client<SrvType>::SharedPtr client, typename SrvType::Request::SharedPtr request, typename std::shared_future<typename SrvType::Response::SharedPtr>& shared_future)
+        {
+            if(srvReqId >= 0)
+            {
+                QMessageBox::warning(uiPanel->mainWidget, "Service call in progress!", "A service call is already in progess. Please try again later.");
+                return;
+            }
+
+            srvReqId = 0;
+
+            std::string srvName = client->get_service_name();
+            updateStatus("Waiting for service " + srvName);
+            if(!client->wait_for_service(1s))
+            {
+                updateStatus("Service unavailable!");
+                srvReqId = -1; //we allowed to call services again
+                return;
+            }
+
+            updateStatus("Making call to service " + srvName);
+            auto future = client->async_send_request(request);
+            srvReqId = future.request_id;
+            shared_future = future.share();
+            //this is how we wait for a service result
+            QTimer::singleShot(250,
+                [this, client, &shared_future] () { this->waitForService<SrvType>(client, shared_future); });
+            auto node = getDisplayContext()->getRosNodeAbstraction().lock()->get_raw_node();
+            clientSendTime = node->get_clock()->now();
+        }
+
+
+        template<typename SrvType>
+        void waitForService(typename rclcpp::Client<SrvType>::SharedPtr client, typename std::shared_future<typename SrvType::Response::SharedPtr>& future)
+        {
+            if(!future.valid())
+            {
+                updateStatus("Service result has become invalid!");
+                srvReqId = -1;
+                return;
+            }
+
+            auto futureStatus = future.wait_for(10ms);
+            if(futureStatus != std::future_status::timeout)
+            {
+                //success
+                typename rclcpp::Client<SrvType>::SharedResponse response = future.get();
+                std::string 
+                    srvName = client->get_service_name(),
+                    successStr = (response->success ? "Succeeded" : "Failed");
+
+                updateStatus("Call to " + srvName + " " + successStr + "; " + response->message);
+                srvReqId = -1;
+                return;
+            }
+
+            //not ready yet
+            auto node = getDisplayContext()->getRosNodeAbstraction().lock()->get_raw_node();
+            rclcpp::Time currentTime = node->get_clock()->now();
+            if(currentTime - clientSendTime > 5s)
+            {
+                updateStatus("Service timed out.");
+                client->remove_pending_request(srvReqId);
+                srvReqId = -1;
+                return;
+            }
+
+            //schedule next check
+            QTimer::singleShot(250,
+                [this, client, &future] () { this->waitForService<SrvType>(client, future); });
+        }
+
         // UI Panel instance
         Ui_Actuators *uiPanel;
 
         //process vars
-        std::string robot_ns;
+        std::string robotNs;
+
+        //status subscriber
+        rclcpp::Subscription<ActuatorStatus>::SharedPtr statusSub;
+
+        //service clients
+        rclcpp::Client<Trigger>::SharedPtr
+            dropperClient,
+            torpedoClient,
+            reloadClient,
+            torpMarkerGoHomeClient,
+            torpMarkerSetHomeClient;
+
+        rclcpp::Client<SetBool>::SharedPtr
+            armClient,
+            clawClient;
+        
+        //service tracking
+        std::shared_future<Trigger::Response::SharedPtr> activeTriggerClientFuture;
+        std::shared_future<SetBool::Response::SharedPtr> activeSetBoolClientFuture;
+        int64_t srvReqId;
+        rclcpp::Time clientSendTime;
     };
 
 } // namespace riptide_rviz
--- riptide_rviz/riptide_rviz/src/Actuators.cpp
+++ riptide_rviz/riptide_rviz/src/Actuators.cpp
@@ -11,12 +11,54 @@ using namespace std::placeholders;
 
 namespace riptide_rviz
 {
+    static const char *CLAW_STATUSES[] = {
+        "Error",
+        "Disarmed",
+        "Unknown",
+        "Opened",
+        "Closed",
+        "Opening",
+        "Closing",
+        "Invalid"
+    };
+
+    static const char *TORPEDO_STATUSES[] = {
+        "Error",
+        "Disarmed",
+        "Charging",
+        "Charged",
+        "Firing",
+        "Fired",
+        "Invalid"
+    };
+
+    static const char *DROPPER_STATUSES[] = {
+        "Error",
+        "Disarmed",
+        "Ready",
+        "Dropping",
+        "Dropped",
+        "Invalid"
+    };
+
+    static const char
+        *STATUS_ERROR_STYLESHEET = "QLabel {color: red;} QLabel:!enabled{color:gray;}",
+        *STATUS_GOOD_STYLESHEET = "QLabel {color: green;} QLabel:!enabled{color:gray;}";
+
+
     Actuators::Actuators(QWidget *parent) : rviz_common::Panel(parent)
     {
         setFocusPolicy(Qt::ClickFocus);
 
         uiPanel = new Ui_Actuators();
         uiPanel->setupUi(this);
+    }
+
+
+    Actuators::~Actuators()
+    {
+        // master window control removal
+        delete uiPanel;
     }
 
 
@@ -29,70 +71,11 @@ namespace riptide_rviz
         connect(uiPanel->ctrlCloseClaw, &QPushButton::clicked, this, &Actuators::handleCloseClaw);
         connect(uiPanel->ctrlFireTorpedo, &QPushButton::clicked, this, &Actuators::handleFireTorpedo);
         connect(uiPanel->ctrlDropMarker, &QPushButton::clicked, this, &Actuators::handleDropMarker);
+        connect(uiPanel->ctrlReload, &QPushButton::clicked, this, &Actuators::handleReload);
         connect(uiPanel->ctrlClawGoHome, &QPushButton::clicked, this, &Actuators::handleClawGoHome);
         connect(uiPanel->ctrlClawSetHome, &QPushButton::clicked, this, &Actuators::handleClawSetHome);
         connect(uiPanel->ctrlTorpGoHome, &QPushButton::clicked, this, &Actuators::handleTorpGoHome);
         connect(uiPanel->ctrlTorpSetHome, &QPushButton::clicked, this, &Actuators::handleTorpSetHome);
-    }
-
-
-    void Actuators::handleArm()
-    {
-        RVIZ_COMMON_LOG_INFO("Arm");
-    }
-
-
-    void Actuators::handleDisarm()
-    {
-        RVIZ_COMMON_LOG_INFO("Disarm");
-    }
-
-
-    void Actuators::handleOpenClaw()
-    {
-        RVIZ_COMMON_LOG_INFO("Open claw");
-    }
-
-
-    void Actuators::handleCloseClaw()
-    {
-        RVIZ_COMMON_LOG_INFO("Close claw");
-    }
-
-
-    void Actuators::handleFireTorpedo()
-    {
-        RVIZ_COMMON_LOG_INFO("Fire Torpedo");
-    }
-
-
-    void Actuators::handleDropMarker()
-    {
-        RVIZ_COMMON_LOG_INFO("Drop Marker");
-    }
-
-
-    void Actuators::handleClawGoHome()
-    {
-        RVIZ_COMMON_LOG_INFO("claw go home");
-    }
-
-
-    void Actuators::handleClawSetHome()
-    {
-        RVIZ_COMMON_LOG_INFO("claw set home");
-    }
-
-
-    void Actuators::handleTorpGoHome()
-    {
-        RVIZ_COMMON_LOG_INFO("torp go home");
-    }
-
-
-    void Actuators::handleTorpSetHome()
-    {
-        RVIZ_COMMON_LOG_INFO("torp set home");
     }
 
 
@@ -101,21 +84,38 @@ namespace riptide_rviz
         rviz_common::Panel::load(config);
 
         // create our value containers
-        QString * str = new QString();
+        QString str;
 
         // load the namesapce param
-        if(config.mapGetString("robot_namespace", str)){
-            robot_ns = str->toStdString();
+        if(config.mapGetString("robot_namespace", &str)){
+            robotNs = str.toStdString();
         } else {
             // default value
-            robot_ns = "/talos";
+            robotNs = "/talos";
             RVIZ_COMMON_LOG_WARNING("ActuatorPanel: Loading default value for 'namespace'");
         }
+
+        RVIZ_COMMON_LOG_INFO("ActuatorPanel: Using namespace " + robotNs);
 
         // get our local rosnode
         auto node = getDisplayContext()->getRosNodeAbstraction().lock()->get_raw_node();
 
-        delete str;
+        //initialize status sub
+        statusSub = node->create_subscription<ActuatorStatus>(robotNs + "/state/actuator/status", rclcpp::SensorDataQoS(),
+            std::bind(&Actuators::statusCallback, this, _1));
+
+        //initialize service information
+        srvReqId = -1; //-1 will mean that no srv is active
+        clientSendTime = node->get_clock()->now();
+
+        //initialize clients
+        dropperClient = node->create_client<Trigger>(robotNs + "/command/actuator/dropper");
+        torpedoClient = node->create_client<Trigger>(robotNs + "/command/actuator/torpedo");
+        reloadClient = node->create_client<Trigger>(robotNs + "/command/actuator/notify_reload");
+        torpMarkerGoHomeClient = node->create_client<Trigger>(robotNs + "/command/actuator/torpedo_marker/go_home");
+        torpMarkerSetHomeClient = node->create_client<Trigger>(robotNs + "/command/actuator/torpedo_marker/set_home");
+        armClient = node->create_client<SetBool>(robotNs + "/command/actuator/arm");
+        clawClient = node->create_client<SetBool>(robotNs + "/command/actuator/claw");
     }
 
 
@@ -124,14 +124,138 @@ namespace riptide_rviz
         rviz_common::Panel::save(config);
 
         // write our config values
-        config.mapSetValue("robot_namespace", QString::fromStdString(robot_ns));
+        config.mapSetValue("robot_namespace", QString::fromStdString(robotNs));
     }
 
 
-    Actuators::~Actuators()
+    void Actuators::handleArm()
     {
-        // master window control removal
-        delete uiPanel;
+        callSetBoolService(armClient, true);
+    }
+
+
+    void Actuators::handleDisarm()
+    {
+        callSetBoolService(armClient, false);
+    }
+
+
+    void Actuators::handleOpenClaw()
+    {
+        callSetBoolService(clawClient, true);
+    }
+
+
+    void Actuators::handleCloseClaw()
+    {
+        callSetBoolService(clawClient, false);
+    }
+
+
+    void Actuators::handleFireTorpedo()
+    {
+        callTriggerService(torpedoClient);
+    }
+
+
+    void Actuators::handleDropMarker()
+    {
+        callTriggerService(dropperClient);
+    }
+
+
+    void Actuators::handleReload()
+    {
+        callTriggerService(reloadClient);
+    }
+
+
+    void Actuators::handleClawGoHome()
+    {
+        QMessageBox::warning(uiPanel->mainWidget, "Not supported", "This action is not supported yet.");
+    }
+
+
+    void Actuators::handleClawSetHome()
+    {
+        QMessageBox::warning(uiPanel->mainWidget, "Not supported", "This action is not supported yet.");
+    }
+
+
+    void Actuators::handleTorpGoHome()
+    {
+        callTriggerService(torpMarkerGoHomeClient);
+    }
+
+
+    void Actuators::handleTorpSetHome()
+    {
+        callTriggerService(torpMarkerSetHomeClient);
+    }
+
+
+    void Actuators::statusCallback(const ActuatorStatus::SharedPtr msg)
+    {
+        //handle arm status
+        bool armed = msg->actuators_armed;
+        
+        //enable or disable controls based on act states
+        uiPanel->ctrlArm->setEnabled(!armed);
+        uiPanel->ctrlDisarm->setEnabled(armed);
+        uiPanel->ctrlOpenClaw->setEnabled(armed && msg->claw_state != ActuatorStatus::CLAW_OPENED && msg->claw_state != ActuatorStatus::CLAW_ERROR);
+        uiPanel->ctrlCloseClaw->setEnabled(armed && msg->claw_state != ActuatorStatus::CLAW_CLOSED && msg->claw_state != ActuatorStatus::CLAW_ERROR);
+        uiPanel->ctrlFireTorpedo->setEnabled(armed && msg->torpedo_state == ActuatorStatus::TORPEDO_CHARGED && msg->torpedo_available_count > 0);
+        uiPanel->ctrlDropMarker->setEnabled(armed && msg->dropper_state == ActuatorStatus::DROPPER_READY && msg->dropper_available_count > 0);
+        uiPanel->ctrlClawGoHome->setEnabled(armed);
+        uiPanel->ctrlClawSetHome->setEnabled(!armed);
+        uiPanel->ctrlTorpGoHome->setEnabled(armed);
+        uiPanel->ctrlTorpSetHome->setEnabled(!armed);
+
+        //handle claw status
+        std::clamp<int>(msg->claw_state, 0, sizeof(CLAW_STATUSES)/sizeof(*CLAW_STATUSES) - 1);
+        uiPanel->lblClawReady->setText(QString::fromStdString(CLAW_STATUSES[msg->claw_state]));
+        uiPanel->lblClawReady->setEnabled(msg->claw_state != ActuatorStatus::CLAW_DISARMED);
+        uiPanel->lblClawReady->setStyleSheet((msg->claw_state == ActuatorStatus::CLAW_ERROR ? STATUS_ERROR_STYLESHEET : STATUS_GOOD_STYLESHEET));
+
+        //handle torpedo status and available count
+        std::clamp<int>(msg->torpedo_state, 0, sizeof(CLAW_STATUSES)/sizeof(*CLAW_STATUSES) - 1);
+        uiPanel->lblTorpedoReady->setText(QString::fromStdString(TORPEDO_STATUSES[msg->torpedo_state]));
+        uiPanel->lblTorpedoReady->setEnabled(msg->torpedo_state != ActuatorStatus::TORPEDO_DISARMED);
+        uiPanel->lblTorpedoReady->setStyleSheet((msg->torpedo_state == ActuatorStatus::TORPEDO_ERROR ? STATUS_ERROR_STYLESHEET : STATUS_GOOD_STYLESHEET));
+        
+        std::string availableString = "Available: " + std::to_string(msg->torpedo_available_count);
+        uiPanel->lblTorpAvailable->setText(QString::fromStdString(availableString));
+
+        //handle dropper status and available count
+        std::clamp<int>(msg->dropper_state, 0, sizeof(CLAW_STATUSES)/sizeof(*CLAW_STATUSES) - 1);
+        uiPanel->lblDropperReady->setText(QString::fromStdString(DROPPER_STATUSES[msg->dropper_state]));
+        uiPanel->lblDropperReady->setEnabled(msg->dropper_state != ActuatorStatus::DROPPER_DISARMED);
+        uiPanel->lblDropperReady->setStyleSheet((msg->dropper_state == ActuatorStatus::DROPPER_ERROR ? STATUS_ERROR_STYLESHEET : STATUS_GOOD_STYLESHEET));
+        
+        availableString = "Available: " + std::to_string(msg->dropper_available_count);
+        uiPanel->lblDropperAvailable->setText(QString::fromStdString(availableString));
+    }
+
+
+    void Actuators::updateStatus(const std::string& status)
+    {
+        RVIZ_COMMON_LOG_INFO(status);
+        uiPanel->statusBrowser->setText(QString::fromStdString(status));
+    }
+
+    
+    void Actuators::callTriggerService(rclcpp::Client<Trigger>::SharedPtr client)
+    {
+        Trigger::Request::SharedPtr request = std::make_shared<Trigger::Request>();
+        callService<Trigger>(client, request, activeTriggerClientFuture);
+    }
+
+    
+    void Actuators::callSetBoolService(rclcpp::Client<SetBool>::SharedPtr client, bool value)
+    {
+        SetBool::Request::SharedPtr request = std::make_shared<SetBool::Request>();
+        request->data = value;
+        callService<SetBool>(client, request, activeSetBoolClientFuture);
     }
 } // namespace riptide_rviz
 
