ca435d8f55b6adc773b7ac70de162dd1bedd1b88
Brach Knutson <brach2003@gmail.com>
2024-07-22 00:40:25 -0400

    added tare button for gyro

--- riptide_rviz/riptide_rviz/src/ElectricalPanel.cpp
+++ riptide_rviz/riptide_rviz/src/ElectricalPanel.cpp
@@ -14,7 +14,7 @@ namespace riptide_rviz
 
         ui = new Ui_ElectricalPanel();
         ui->setupUi(this);
-        ui->errLabel->setText("");
+        setStatus("", false);
         loaded = false;
     }
 
@@ -41,9 +41,12 @@ namespace riptide_rviz
         pub = node->create_publisher<riptide_msgs2::msg::ElectricalCommand>(topicName, 10);
 
         //make the action client for the imu mag cal
-        std::string fullActionName = robotNs.toStdString() + CALIB_ACTION_NAME;
-        imuCalClient = rclcpp_action::create_client<MagCal>(node, fullActionName);
+        std::string 
+            fullMagCalActionName = robotNs.toStdString() + MAG_CAL_ACTION_NAME,
+            fullTareGyroActionName = robotNs.toStdString() + TARE_GYRO_ACTION_NAME;
 
+        imuCalClient = rclcpp_action::create_client<MagCal>(node, fullMagCalActionName);
+        tareGyroClient = rclcpp_action::create_client<TareGyro>(node, fullTareGyroActionName);
         loaded = true;
     }
 
@@ -57,39 +60,42 @@ namespace riptide_rviz
 
     void ElectricalPanel::onInitialize()
     {
-        connect(ui->commandSend, &QPushButton::clicked, this, &ElectricalPanel::sendCommand);
+        connect(ui->commandSend, &QPushButton::clicked, this, &ElectricalPanel::sendElectricalCommand);
         connect(ui->magCalSend, &QPushButton::clicked, this, &ElectricalPanel::sendMagCal);
+        connect(ui->commandTareFog, &QPushButton::clicked, this, &ElectricalPanel::sendTareGyro);
 
         //initial UI state
         ui->calibProgress->setValue(0);
-        ui->errLabel->setText("");
+        setStatus("", false);
         ui->magCalSend->setText("Calibrate");
     }
 
 
-    void ElectricalPanel::sendCommand()
+    void ElectricalPanel::sendElectricalCommand()
     {
         if(loaded)
         {
             riptide_msgs2::msg::ElectricalCommand msg;
             msg.command = ui->commandSelect->currentIndex();
             pub->publish(msg);
-            ui->errLabel->setText("");
+            setStatus("", false);
         } else 
         {
-            ui->errLabel->setText("Panel not loaded! Please save your config and restart RViz");
+            setStatus("Panel not loaded! Please save your config and restart RViz", true);
         }
     }
 
-    void ElectricalPanel::sendMagCal(){
+
+    void ElectricalPanel::sendMagCal()
+    {
         if(! loaded){
-            ui->errLabel->setText("Panel not loaded! Please save your config and restart RViz");
+            setStatus("Panel not loaded! Please save your config and restart RViz", true);
             return;
         }
 
-        if(calInProgress){
+        if(imuCalInProgress){
             imuCalClient->async_cancel_all_goals();
-            ui->errLabel->setText("Cancelling calibration request");
+            setStatus("Cancelling calibration request", false);
             return;
         }
 
@@ -99,45 +105,97 @@ namespace riptide_rviz
 
         // make sure the cal server is online
         if(!imuCalClient->wait_for_action_server(1s)){
-            RVIZ_COMMON_LOG_ERROR("ElectricalPanel: Mag calibration action server not available!");
-            ui->errLabel->setText("Calibration server unavailiable!");
+            setStatus("Calibration server unavailiable!", true);
             return;
         }
 
         // create and send goal
         MagCal::Goal cal_goal;
         MagSendGoalOptions options;
-        options.goal_response_callback  = std::bind(&ElectricalPanel::goalResponseCb, this, _1);
-        options.feedback_callback       = std::bind(&ElectricalPanel::feedbackCb, this, _1, _2);
-        options.result_callback         = std::bind(&ElectricalPanel::resultCb, this, _1);
+        options.goal_response_callback  = std::bind(&ElectricalPanel::magCalGoalResponseCb, this, _1);
+        options.feedback_callback       = std::bind(&ElectricalPanel::magCalFeedbackCb, this, _1, _2);
+        options.result_callback         = std::bind(&ElectricalPanel::magCalResultCb, this, _1);
         imuCalClient->async_send_goal(cal_goal, options);
-        calInProgress = true;
+        imuCalInProgress = true;
 
         ui->magCalSend->setText("Cancel");
     }
 
-    void ElectricalPanel::goalResponseCb(const MagGoalHandle::SharedPtr & goal_handle){
+
+    void ElectricalPanel::sendTareGyro()
+    {
+        if(! loaded){
+            setStatus("Panel not loaded! Please save your config and restart RViz", true);
+            return;
+        }
+
+        if(gyroTareInProgress){
+            tareGyroClient->async_cancel_all_goals();
+            setStatus("Cancelling tare request", false);
+            return;
+        }
+        
+        setStatus("Sending gyro tare goal", false);
+
+        // reset the panel state for cal
+        ui->calibProgress->setValue(0);
+
+        // make sure the cal server is online
+        if(!tareGyroClient->wait_for_action_server(1s)){
+            setStatus("Tare server unavailiable!", true);
+            return;
+        }
+
+        // create and send goal
+        TareGyro::Goal tare_goal;
+        tare_goal.num_samples = ui->tareSamples->value();
+        tare_goal.timeout_seconds = ui->tareTimeout->value();
+        TareGyroSendGoalOptions options;
+        options.goal_response_callback  = std::bind(&ElectricalPanel::tareGyroGoalResponseCb, this, _1);
+        options.result_callback         = std::bind(&ElectricalPanel::tareGyroResultCb, this, _1);
+        tareGyroClient->async_send_goal(tare_goal, options);
+        ui->commandTareFog->setText("Cancel");
+    }
+
+
+    void ElectricalPanel::setStatus(const QString& status, bool error)
+    {
+        ui->errLabel->setText(status);
+        ui->errLabel->setStyleSheet(error ? "color: red" : "");
+
+        if(error)
+        {
+            RVIZ_COMMON_LOG_ERROR(status.toStdString());
+        } else
+        {
+            RVIZ_COMMON_LOG_INFO(status.toStdString());
+        }
+    }
+
+
+    void ElectricalPanel::magCalGoalResponseCb(const MagGoalHandle::SharedPtr & goal_handle){
         if(goal_handle)
         {
             switch(goal_handle->get_status())
             {
                 case GOAL_STATE_ACCEPTED:
-                    ui->errLabel->setText("");
+                    setStatus("Performing mag cal", false);
                     break;
                 case GOAL_STATE_CANCELING:
-                    ui->errLabel->setText("Canceling calibration...");
+                    setStatus("Canceling calibration...", false);
                     break;
                 default:
-                    ui->errLabel->setText("Unknown goal state");
+                    setStatus("Unknown goal state", true);
                     break;
             }
         } else
         {
-            ui->errLabel->setText("Calibration request rejected!");
+            setStatus("Calibration request rejected!", true);
         }   
     }
 
-    void ElectricalPanel::feedbackCb( MagGoalHandle::SharedPtr,
+
+    void ElectricalPanel::magCalFeedbackCb( MagGoalHandle::SharedPtr,
         const std::shared_ptr<const MagCal::Feedback> feedback){
 
 
@@ -160,25 +218,71 @@ namespace riptide_rviz
         ui->calibProgress->setValue((int)disp_var);
     }
 
-    void ElectricalPanel::resultCb(const MagGoalHandle::WrappedResult & result){
+
+    void ElectricalPanel::magCalResultCb(const MagGoalHandle::WrappedResult & result){
         switch(result.code)
         {
             case rclcpp_action::ResultCode::SUCCEEDED:
                 ui->calibProgress->setValue(100);
                 break;
             case rclcpp_action::ResultCode::ABORTED:
-                ui->errLabel->setText("Calibration aborted, Examine driver logs for info");
+                setStatus("Calibration aborted, Examine driver logs for info", true);
                 break;
             case rclcpp_action::ResultCode::CANCELED:
-                ui->errLabel->setText("Calibration canceled!");
+                setStatus("Calibration canceled!", false);
                 break;
             case rclcpp_action::ResultCode::UNKNOWN:
-                ui->errLabel->setText("Uknown calibration result recieved???");
+                setStatus("Uknown calibration result recieved???", true);
                 break;
         }
 
-        calInProgress = false;
+        imuCalInProgress = false;
         ui->magCalSend->setText("Calibrate");
+    }
+
+
+    void ElectricalPanel::tareGyroGoalResponseCb(const TareGyroGoalHandle::SharedPtr & goal_handle){
+        if(goal_handle)
+        {
+            switch(goal_handle->get_status())
+            {
+                case GOAL_STATE_ACCEPTED:
+                    setStatus("Performing gyro tare", false);
+                    gyroTareInProgress = true;
+                    break;
+                case GOAL_STATE_CANCELING:
+                    setStatus("Canceling tare", false);
+                    break;
+                default:
+                    setStatus("Unknown goal state", true);
+                    break;
+            }
+        } else
+        {
+            setStatus("Tare request rejected!", true);
+        }   
+    }
+
+
+    void ElectricalPanel::tareGyroResultCb(const TareGyroGoalHandle::WrappedResult & result){
+        switch(result.code)
+        {
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                ui->calibProgress->setValue(100);
+                break;
+            case rclcpp_action::ResultCode::ABORTED:
+                setStatus("Tare aborted: " + QString::fromStdString(result.result->result), true);
+                break;
+            case rclcpp_action::ResultCode::CANCELED:
+                setStatus("Tare canceled!", false);
+                break;
+            case rclcpp_action::ResultCode::UNKNOWN:
+                setStatus("Unknown tare result recieved???", true);
+                break;
+        }
+
+        gyroTareInProgress = false;
+        ui->commandTareFog->setText("Calibrate");
     }
 }
 
