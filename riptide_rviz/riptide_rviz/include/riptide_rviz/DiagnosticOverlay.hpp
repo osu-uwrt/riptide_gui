931fd5186dd755ecb8557424c3b6a1ac7be8d9e7
Effo12345 <4p7septipuses@gmail.com>
2024-08-09 01:22:38 -0400

    Report leak sensor detections

--- riptide_rviz/riptide_rviz/include/riptide_rviz/DiagnosticOverlay.hpp
+++ riptide_rviz/riptide_rviz/include/riptide_rviz/DiagnosticOverlay.hpp
@@ -7,6 +7,7 @@
 #include <diagnostic_msgs/msg/diagnostic_array.hpp>
 #include <std_msgs/msg/bool.hpp>
 #include <sensor_msgs/msg/temperature.hpp>
+#include <std_msgs/msg/bool.hpp>
 
 #include <rviz_common/properties/string_property.hpp>
 #include <rviz_common/properties/float_property.hpp>
@@ -31,6 +32,7 @@ namespace riptide_rviz
         void diagnosticCallback(const diagnostic_msgs::msg::DiagnosticArray & msg);
         void killCallback(const std_msgs::msg::Bool & msg);
         void zedCallback(const sensor_msgs::msg::Temperature& msg);
+        void leakCallback(const std_msgs::msg::Bool& msg);
 
         void checkTimeout();
 
@@ -44,20 +46,23 @@ namespace riptide_rviz
         rclcpp::TimerBase::SharedPtr checkTimer;
 
         // times for stamping
-        rclcpp::Time lastDiag, lastKill, lastZed;
-        bool diagsTimedOut, killTimedOut, zedTimedOut;
+        rclcpp::Time lastDiag, lastKill, lastZed, lastLeak;
+        bool diagsTimedOut, killTimedOut, zedTimedOut, leakTimedOut;
+        bool startedLeaking = false;
 
 
         // subscription for diagnostics
         rclcpp::Subscription<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr diagSub;
         rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr killSub;
         rclcpp::Subscription<sensor_msgs::msg::Temperature>::SharedPtr zedSub;
+        rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr leakSub;
 
         // ids for rendering items so that we can edit them
         int voltageTextId = -1;
         int diagLedConfigId = -1;
         int killLedConfigId = -1;
         int zedLedConfigId = -1;
+        int leakLedConfigId = -1;
 
         // font configuration info
         QStringList fontFamilies;
@@ -81,6 +86,11 @@ namespace riptide_rviz
         };
         PaintedCircleConfig zedLedConfig = {
             100, 50, 0, 0, 7, 9,
+            QColor(255, 0, 255, 255),
+            QColor(0, 0, 0, 255)
+        };
+        PaintedCircleConfig leakLedConfig = {
+            20, 100, 0, 0, 7, 9,
             QColor(255, 0, 255, 255),
             QColor(0, 0, 0, 255)
         };
